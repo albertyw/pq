26f43535f0ba244ec85d00d76ed0797b1202bbda
Albert Wang <albertyw@mit.edu>
2016-12-21 10:35:59 -0800

    Add pq.py

---
+++ pq.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+import fileinput
+
+
+def read_input():
+    data = ''
+    for line in fileinput.input():
+        data += line
+    return data
+
+
+def newline(symbol, indentation):
+    print_output(symbol)
+    print_output("\n")
+    spacing = ' ' * indentation * 2
+    print_output(spacing)
+
+
+def format_parens(data):
+    indentation = 0
+    symbol = ''
+    for char in data:
+        if char.strip() == '':
+            newline(symbol, indentation)
+            symbol = ''
+        if char in [')']:
+            indentation -= 1
+            newline(symbol, indentation)
+            symbol = ''
+        symbol += char.strip()
+        if char in ['(']:
+            indentation += 1
+            newline(symbol, indentation)
+            symbol = ''
+    if indentation != 0:
+        indentation = 0
+        newline('', indentation)
+
+
+def print_output(output):
+    print(output, end='')
+
+
+format_parens(read_input())
